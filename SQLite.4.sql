8214289f613682defc9a00cdbb41765dc3634273
Handy Milliance <milliance23@gmail.com>
2022-07-17 13:11:46 -0300

    Adicionando arquivos...

---
+++ SQLite.4.sql
@@ -0,0 +1,32 @@
+-- Questão 1
+SELECT cliente.id, 
+	   cliente.nome, 
+       cliente_conta.id_cliente,
+       cliente_conta.dependente
+FROM cliente
+JOIN cliente_conta ON cliente.id = cliente_conta.id_cliente
+where dependente = 1
+GROUP BY cliente_conta.dependente, cliente.nome 
+
+
+-- Questão 2 MAIORES TRANSACOES
+select id_cliente_conta, valor AS "MAIORES TRANSACOES" FROM transacao
+ORDER BY valor desc
+limit(5);
+
+
+-- Questão 2 MENORES TRANSACOES
+select id_cliente_conta, valor AS "MENORES TRANSACOES" FROM transacao
+ORDER BY valor asc
+limit(5);
+
+
+-- Questão 3
+select id_cliente_conta, 
+	   valor, 
+       descricao, 
+       SUM(valor) AS "Saldo"
+FROM transacao 
+JOIN tipo_transacao oN transacao.id_tipo_transacao = tipo_transacao.id
+WHERE id_tipo_transacao = 1 GROUP BY id_cliente_conta ORDER BY id_cliente_conta
+ 
